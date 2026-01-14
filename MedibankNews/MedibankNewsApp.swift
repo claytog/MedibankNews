5a6af057cf509a9afd5880253085ba9b4f3693ad
claytog <claytongilbert@gmail.com>
2026-01-14 20:54:02 +1100

    Initial Commit

---
+++ MedibankNews/MedibankNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedibankNewsApp.swift
+//  MedibankNews
+//
+//  Created by Clayton on 14/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct MedibankNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
